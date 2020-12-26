278a7ee9e51323c9d23c184d24572d66365c6bdc
hashim <hashimtahir@live.com>
2020-12-26 17:20:06 +0500

    Splash Sceeen added.

--- app/src/main/java/com/hashim/simpleshortexamples/MainActivity.kt
+++ app/src/main/java/com/hashim/simpleshortexamples/MainActivity.kt
@@ -5,10 +5,6 @@
 package com.hashim.simpleshortexamples
 
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.hashim.simpleshortexamples.databinding.ActivityMainBinding
 
@@ -17,32 +13,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var hActivityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_SimpleShortExamples)
+
         hActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(hActivityMainBinding.root)
 
-        hSetupListeners()
-
-    }
-
-    private fun hSetupListeners() {
-        registerForContextMenu(hActivityMainBinding.hOpenMenuB)
-        hActivityMainBinding.hOpenMenuB.setOnClickListener {
-            openContextMenu(hActivityMainBinding.hOpenMenuB)
-        }
-    }
-
-
-    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
-        menuInflater.inflate(R.menu.floating_menu, menu)
-    }
-
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.hItem1 -> Toast.makeText(this, "item 1,", Toast.LENGTH_LONG).show()
-            R.id.hItem2 -> Toast.makeText(this, "item 2,", Toast.LENGTH_LONG).show()
-            R.id.hItem3 -> Toast.makeText(this, "item 3,", Toast.LENGTH_LONG).show()
-            R.id.hItem4 -> Toast.makeText(this, "item 4,", Toast.LENGTH_LONG).show()
-        }
-        return super.onContextItemSelected(item)
     }
 }

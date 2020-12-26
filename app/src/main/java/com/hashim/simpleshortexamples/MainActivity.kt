9be9b351b49704e43e7537514ee0dcc8fb09610c
hashim <hashimtahir@live.com>
2020-12-26 13:36:28 +0500

    Floting menu added. View binding initilized.

--- app/src/main/java/com/hashim/simpleshortexamples/MainActivity.kt
+++ app/src/main/java/com/hashim/simpleshortexamples/MainActivity.kt
@@ -4,12 +4,43 @@
 
 package com.hashim.simpleshortexamples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.hashim.simpleshortexamples.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var hActivityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        hActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(hActivityMainBinding.root)
+
+        hRegisterForContextMenu()
+    }
+
+    private fun hRegisterForContextMenu() {
+        hActivityMainBinding.hOpenMenuB.setOnClickListener {
+
+        }
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.floating_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.hItem1 -> Toast.makeText(this, "item 1,", Toast.LENGTH_LONG).show()
+            R.id.hItem2 -> Toast.makeText(this, "item 2,", Toast.LENGTH_LONG).show()
+            R.id.hItem3 -> Toast.makeText(this, "item 3,", Toast.LENGTH_LONG).show()
+            R.id.hItem4 -> Toast.makeText(this, "item 4,", Toast.LENGTH_LONG).show()
+        }
+        return super.onContextItemSelected(item)
     }
 }
